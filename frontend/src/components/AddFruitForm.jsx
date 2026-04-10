4c189a7d19140b1c9eafcbfdad0f530b42c734a6
Gowtham <jayavarapugowtham@gmail.com>
2026-04-10 18:02:03 +0530

    lets start integrating frontend and backend

---
+++ frontend/src/components/AddFruitForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddFruitForm = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AddFruitForm
---
+++ frontend/src/components/Fruits.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import AddFruitForm from "./AddFruitForm";
+import api from "../api";
+
+const FruitList = () => {
+  const [fruits, setFruits] = useState([]);
+  const fetchFruits = async () => {
+    try {
+      const response = await api.get("/fruits");
+      setFruits(response.data.fruits);
+    } catch (error) {
+      console.error("Error fetching fruits", error);
+    }
+  };
+  const addFruit = async (fruitName) => {
+    try {
+      await api.post("/fruits", { name: fruitName });
+      fetchFruits();
+    } catch (error) {
+      console.error("error adding fruit", error);
+    }
+  };
+  useEffect(() => {
+    fetchFruits();
+  }, []);
+  return (
+    <div>
+      <h2>Fruits list</h2>
+      <ul>
+        {fruits.map((fruit, index) => (
+          <li key={index}>{fruit.name}</li>
+        ))}
+      </ul>
+      <AddFruitForm addFruit={addFruit} />
+    </div>
+  );
+};
+
+export default FruitList;
